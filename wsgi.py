f38a663c52a88ef8df1c57b0f95cd765a01082be
Douglas Schilling Landgraf <dougsland@gmail.com>
2019-10-09 11:30:08 -0400

    testing

--- wsgi.py
+++ wsgi.py
@@ -189,33 +189,33 @@ async def recommendations(msg_id: str, message: dict):
     logger.info("==========hosts type==========")
 
     logger.info(hosts)
-    for host_info in hosts['rhv-log-collector-analyzer']:
-        hits = []
+    # for host_info in hosts['rhv-log-collector-analyzer']:
+    hits = []
 
-        logger.info("++++++++hostinfo++++++++++")
-        logger.info(host_info)
-        logger.info("++++++++hostinfo++++++++++")
-        hits = await hits_with_rules(hosts)
+    logger.info("++++++++message++++++++++")
+    logger.info(message)
+    logger.info("++++++++message++++++++++")
+    hits = await hits_with_rules(hosts)
 
-        host_id = create_host(
-            host_info["account"],
-            host_info["metadata"]["insights_id"],
-            host_info["metadata"]["bios_uuid"],
-            host_info["metadata"]["fqdn"],
-            host_info["metadata"]["ip_addresses"],
-        )
-        logger.info("host id: {0}".format(host_id))
+    host_id = create_host(
+        message["account"],
+        message["metadata"]["insights_id"],
+        message["metadata"]["bios_uuid"],
+        message["metadata"]["fqdn"],
+        message["metadata"]["ip_addresses"],
+    )
+    logger.info("host id: {0}".format(host_id))
 
-        output = {
-            'source': 'rhvanalyzer',
-            'host_product': 'OCP',
-            'host_role': 'Cluster',
-            'inventory_id': host_id,
-            'account': host_info['account'],
-            'hits': hits
-        }
-        output = json.dumps(output).encode()
-        logger.info("JSON {0}".format(output))
+    output = {
+        'source': 'rhvanalyzer',
+        'host_product': 'OCP',
+        'host_role': 'Cluster',
+        'inventory_id': host_id,
+        'account': message['account'],
+        'hits': hits
+    }
+    output = json.dumps(output).encode()
+    logger.info("JSON {0}".format(output))
 
     # Produce message constituting the json
     try:
